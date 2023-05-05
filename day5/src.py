ab9c78ccf790b9911306736f6c883d80048809a6
aiell0 <lawrence.aiello@laiello.com>
2023-05-05 10:33:46 -0400

    feat: day 5 part two

--- day5/src.py
+++ day5/src.py
@@ -1,7 +1,8 @@
 import re
+import copy
 
-# PART ONE
-stack_dict = {}
+part_one_dict = {}
+
 with open("input.txt", "r") as file:
     lines = file.readlines()
     stripped_lines = [i.rstrip("\n") for i in lines]
@@ -11,7 +12,7 @@ with open("input.txt", "r") as file:
     for row in range(empty_row - 1, -1, -1):
         # only for first line, build dictionary
         if row == (empty_row - 1):
-            stack_dict = {i: [] for i in stripped_lines[row] if i != " "}
+            part_one_dict = {i: [] for i in stripped_lines[row] if i != " "}
         # else build stacks in dictionary
         else:
             split_row = stripped_lines[row].split(' ')
@@ -22,7 +23,9 @@ with open("input.txt", "r") as file:
             for i, crate in enumerate(split_row):
                 search_crate = re.search(r'[[A-Z](?=])', crate)
                 if search_crate:
-                    stack_dict[str(i+1)].append(search_crate.group())
+                    part_one_dict[str(i+1)].append(search_crate.group())
+
+    part_two_dict: dict = copy.deepcopy(part_one_dict)
 
     for row in range(empty_row+1, len(stripped_lines)):
         split_row = stripped_lines[row].split(" ")
@@ -30,7 +33,14 @@ with open("input.txt", "r") as file:
         fr: str = str(split_row[3])
         to: str = str(split_row[5])
 
+        # PART ONE
         for i in range(int(move)):
-            stack_dict[to].append(stack_dict[fr].pop())
+            part_one_dict[to].append(part_one_dict[fr].pop())
 
-    print("answer: ", [stack_dict[key][len(stack_dict[key])-1] for key in stack_dict.keys()])
+        # PART TWO
+        part_two_dict[to].extend(part_two_dict[fr][-int(move):])
+        del part_two_dict[fr][-int(move):]
+
+    print("Part One Answer: ", [part_one_dict[key][len(part_one_dict[key])-1] for key in part_one_dict.keys()])
+    print("Part Two Answer: ", [part_two_dict[key][len(part_two_dict[key])-1] for key in part_two_dict.keys()])
+
